2c97b73af44ebbd149381d1a560b86a5734d4c28
sashka <144311184+iamsashka@users.noreply.github.com>
2024-03-31 17:28:32 +0300

    Update Country.cs

--- MagazinCosmetiki1/Country.cs
+++ MagazinCosmetiki1/Country.cs
@@ -24,6 +24,6 @@ namespace MagazinCosmetiki1
         public string CountryName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Products> Products { get; set; }
+        public virtual ICollection<Products> Products {private get; set; }
     }
 }
