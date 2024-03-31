59d0740c4fa2149b92f7b7d57060c1959a900bc5
sashka <144311184+iamsashka@users.noreply.github.com>
2024-03-31 17:26:23 +0300

    Update MainWindow.xaml.cs

--- MagazinCosmetiki1/MainWindow.xaml.cs
+++ MagazinCosmetiki1/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Windows;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using MagazinCosmetiki1;
@@ -16,17 +16,17 @@ namespace MagazinCosmetiki1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            PageFrame.Content = new Categories();
+            PageFrame.Content = new Categories2();
         }
 
         private void Button_Click1(object sender, RoutedEventArgs e)
         {
-            PageFrame.Content = new Products();
+            PageFrame.Content = new ProductsPage();
         }
 
         private void Button_Click2(object sender, RoutedEventArgs e)
         {
-            PageFrame.Content = new Country();
+            PageFrame.Content = new Country1();
         }
 
         private void PageFrame_Navigated(object sender, NavigationEventArgs e)
